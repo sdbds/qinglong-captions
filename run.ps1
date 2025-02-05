b6dba6636b676ccb3d8b261cc77a85de23e1fa3b
sdbds <865105819@qq.com>
2025-02-06 02:09:01 +0800

    fix name

--- run.ps1
+++ run.ps1
@@ -1,6 +1,5 @@
 $dataset_path = "./datasets"
 $gemini_api_key = ""
-$gemini_model_path = "gemini-2_0-pro-exp-02-05"
 $pixtral_api_key = ""
 $gemini_model_path = "gemini-2.0-pro-exp-02-05"
 $pixtral_model_path = "pixtral-large-2411"
